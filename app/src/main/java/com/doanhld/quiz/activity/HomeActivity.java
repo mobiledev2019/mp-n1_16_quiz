8977887718e6cb3ec386267b46680a7d378d4218
doanhchelsea <leducdoanh.1203>
2019-04-20 00:34:21 +0700

    update layout

---
+++ app/src/main/java/com/doanhld/quiz/activity/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.doanhld.quiz.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+import com.doanhld.quiz.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        CardView play = findViewById(R.id.cardview1);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent playIntent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(playIntent);
+            }
+        });
+    }
+}
